356787193980c2fcc8da3ca65bbe3d5f99a25890
n.roshchuk <nroshchuk@visualfabriq.com>
2022-02-14 14:45:43 +0200

    Implementing sign-in, sigh-up with email and password

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './custom-button.styles.scss'
 
 export const Button = ({ children, isGoogleSignIn, ...props }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...props}>
+    <button type='button' className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...props}>
         {children}
     </button>
 )
